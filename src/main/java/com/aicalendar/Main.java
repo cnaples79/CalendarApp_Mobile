e5b312f116f4def838a36738481ee1427ff8da91
Chase Naples <cnaples79@gmail.com>
2025-07-05 02:57:25 -0400

    Update Main.java

--- src/main/java/com/aicalendar/Main.java
+++ src/main/java/com/aicalendar/Main.java
@@ -4,11 +4,15 @@ import com.aicalendar.views.CalendarView;
 import com.aicalendar.views.ChatView;
 import com.aicalendar.views.TimelineView;
 import com.gluonhq.charm.glisten.application.AppManager;
+import com.gluonhq.charm.glisten.application.DrawerManager;
+import com.gluonhq.charm.glisten.mvc.Drawer;
 import com.gluonhq.charm.glisten.visual.Swatch;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
 
 import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
 
@@ -18,18 +22,19 @@ public class Main extends Application {
     public static final String CHAT_VIEW = "Chat";
     public static final String TIMELINE_VIEW = "Timeline";
 
-    private final AppManager appManager = AppManager.initialize(this::postInit);
-
     @Override
     public void init() {
-        appManager.addViewFactory(CALENDAR_VIEW, () -> new CalendarView());
-        appManager.addViewFactory(CHAT_VIEW, () -> new ChatView());
-        appManager.addViewFactory(TIMELINE_VIEW, () -> new TimelineView());
+        AppManager.initialize(this::postInit);
+        AppManager.getInstance().addViewFactory(CALENDAR_VIEW, () -> new CalendarView());
+        AppManager.getInstance().addViewFactory(CHAT_VIEW, () -> new ChatView());
+        AppManager.getInstance().addViewFactory(TIMELINE_VIEW, () -> new TimelineView());
+
+        DrawerManager.getInstance().setDrawer(new Drawer());
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        appManager.start(primaryStage);
+        AppManager.getInstance().start(primaryStage);
     }
 
     private void postInit(Scene scene) {
